310e9a4b30610c5343db1fe78d8efb04657cd3a1
dhrjrddy <dhrjrddy@gmail.com>
2015-09-03 14:04:33 +0530

    updated with indentation

--- src/com/game/Board.java
+++ src/com/game/Board.java
@@ -1,116 +1,106 @@
 package com.game;
 
 public class Board {
-    private int[][] mines;
-    private char[][] boardGame;
-    private int line, column;
-    private int count=100;
-    private Display output;
-    
-    public Board (){
-        mines = new int[10][10];
-        boardGame = new char[10][10];
-        startMines();
-        randomMines();
-        fillNeighbours();
-        startBoard();
-        output= new Display();
-        
-    }
- /*  
-    public boolean win(){
-        int count=0;
-        for(int line = 0 ; line < 10 ; line++)
-            for(int column = 0 ; column < 10 ; column++)
-                if(boardgame[line][column]=='_')
-                    count++;
-        if(count == 10)
-            return true;
-        else
-            return false;                
-    }
-   */ 
-    public int openNeighbors(){
-        for(int i=-1 ; i<2 ; i++)
-            for(int j=-1 ; j<2 ; j++)
-                if((line+i)!= -1 && (line+i)!= 10 && (column+j) != -1 && (column+j) != 10) 
-                	if (mines[line+i][column+j] != -1){
-                		if (boardGame[line+i][column+j]=='_')
-                			count--;
-                    	boardGame[line+i][column+j]=Character.forDigit(mines[line+i][column+j], 10);                    	
-                	}
-        if(count==10){
-            output.won();
-        }
-        return count;
-    }
-    
-    public boolean inputPosition(int line , int column){
-    		this.line=line;
-    		this.column=column;         
-            if(mines[line][column]== -1){
-            	output.lost();
-                return true;
-            }
-            else{
-                return false;}
-            
-    }
-    
-   
-    public void show(){
-        output.dataDisplay(boardGame,mines);
-    }
-    
-    public void fillNeighbours(){
-        for(int line=0 ; line < 10 ; line++)
-            for(int column=0 ; column < 10 ; column++){
+	private int[][] mines;
+	private char[][] boardGame;
+	private int line;
+	private int column;
+	private int count = 100;
 
-                    for(int i=-1 ; i<=1 ; i++)
-                        for(int j=-1 ; j<=1 ; j++)
-                            if(mines[line][column] != -1 && ((line+i)!= -1 && (line+i)!= 10 && (column+j) != -1 && (column+j) != 10))
-                                if(mines[line+i][column+j] == -1)
-                                    mines[line][column]++;               
-            }
-            
-    }
-/*    
-    public void showMines(){
-        for(int i=0 ; i < 10; i++)
-            for(int j=0 ; j < 10 ; j++)
-                if(mines[i][j] == -1)
-                    boardgame[i][j]='*';
-        
-        show();
-    }
-   */ 
-    public void startBoard(){
-        for(int i=0 ; i<mines.length ; i++)
-            for(int j=0 ; j<mines.length ; j++)
-                boardGame[i][j]= '_';
-    }
-    
-    public void startMines(){
-        for(int i=0 ; i<mines.length ; i++)
-            for(int j=0 ; j<mines.length ; j++)
-                mines[i][j]=0;
-    }
-    
-    public void randomMines(){
-        boolean avilable;
-        int line, column;
-        for(int i=0 ; i<10 ; i++){
-            
-            do{
-                line = (int)(Math.random() * 10) ;
-                column = (int)(Math.random() * 10);
-                if(mines[line][column] == -1)
-                	avilable=true;
-                else
-                	avilable = false;
-            }while(avilable);
-            
-            mines[line][column] = -1;
-        }
-    }
+	public Board() {
+		mines = new int[10][10];
+		boardGame = new char[10][10];
+		startMines();
+		randomMines();
+		fillNeighbours();
+	}
+
+	public char[][] openNeighbors(char[][] boardGame) {
+		this.boardGame = boardGame;
+		for (int i = -1; i < 2; i++) {
+			for (int j = -1; j < 2; j++) {
+				if ((line + i) != -1 && (line + i) != 10 && (column + j) != -1
+						&& (column + j) != 10) {
+					if (mines[line + i][column + j] != -1) {
+						if (boardGame[line + i][column + j] == '_')
+							count--;
+						boardGame[line + i][column + j] = Character.forDigit(
+								mines[line + i][column + j], 10);
+					}
+				}
+			}
+		}
+		return boardGame;
+	}
+
+	public boolean hasMine(int line, int column) {
+		this.line = line;
+		this.column = column;
+		if (mines[line][column] == -1) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	public int noOfBlocks() {
+		return count;
+	}
+
+	public void fillNeighbours() {
+		for (int line = 0; line < 10; line++)
+			for (int column = 0; column < 10; column++) {
+
+				for (int i = -1; i <= 1; i++) {
+					for (int j = -1; j <= 1; j++) {
+						if (mines[line][column] != -1
+								&& ((line + i) != -1 && (line + i) != 10
+										&& (column + j) != -1 && (column + j) != 10)) {
+							if (mines[line + i][column + j] == -1) {
+								mines[line][column]++;
+							}
+						}
+					}
+				}
+			}
+
+	}
+
+	public char[][] showMines() {
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				if (mines[i][j] == -1) {
+					boardGame[i][j] = '*';
+				}
+			}
+		}
+		return boardGame;
+	}
+
+	public void startMines() {
+		for (int i = 0; i < mines.length; i++) {
+			for (int j = 0; j < mines.length; j++) {
+				mines[i][j] = 0;
+			}
+		}
+	}
+
+	public void randomMines() {
+		boolean avilable;
+		int line, column;
+		for (int i = 0; i < 10; i++) {
+			do {
+				line = (int) (Math.random() * 10);
+				column = (int) (Math.random() * 10);
+				if (mines[line][column] == -1) {
+					avilable = true;
+				} else {
+					avilable = false;
+				}
+			} while (avilable);
+
+			mines[line][column] = -1;
+		}
+	}
 }
--- src/com/game/Minesweeper.java
+++ src/com/game/Minesweeper.java
@@ -1,35 +1,74 @@
 package com.game;
 
 import java.util.Scanner;
+
 public class Minesweeper {
-	
+
 	private Board board;
-    
-    public Minesweeper(){
-        board = new Board();
-        play(board);
-    }
-    
-    public void play(Board board){
-    	boolean inputmine = false;
-    	int Line, Column;
-    	int blockleft=100;
-        Scanner input = new Scanner(System.in);
-        do{
-            board.show();
-            System.out.print("row: "); 
-            Line = input.nextInt();
-            System.out.print("column: "); 
-            Column = input.nextInt();
-            inputmine = board.inputPosition(Line , Column);            
-            if(!inputmine){
-            	blockleft=board.openNeighbors();
-            }
-            
-        }while(!inputmine && blockleft!=10);
-        input.close();
-    }
+	private char[][] boardGame;
+	private int turn = 0;
+	private int blockLeft = 100;
+
+	public Minesweeper() {
+		board = new Board();
+		boardGame = new char[10][10];
+		startBoard();
+	}
+
+	public void showBoard() {
+		System.out.println("Turn " + turn);
+		for (int line = 0; line < 10; line++) {
+			for (int column = 0; column < 10; column++) {
+				System.out.print("   " + boardGame[line][column]);
+			}
+			System.out.println();
+		}
+	}
+
+	public void startBoard() {
+		for (int line = 0; line < 10; line++) {
+			for (int column = 0; column < 10; column++) {
+				boardGame[line][column] = '_';
+			}
+		}
+	}
+
+	public void selectCoordinates() {
+		boolean isMine = false;
+		int line;
+		int column;
+		Scanner sc = new Scanner(System.in);
+		do {
+			showBoard();
+			turn++;
+			System.out.print("row: ");
+			line = sc.nextInt();
+			System.out.print("column: ");
+			column = sc.nextInt();
+			isMine = board.hasMine(line - 1, column - 1);
+			if (!isMine) {
+				boardGame = board.openNeighbors(boardGame);
+				blockLeft = board.noOfBlocks();
+			}
+		} while (!isMine && blockLeft != 10);
+		sc.close();
+		showResult();
+	}
+
+	public void showResult() {
+		if (blockLeft == 10) {
+			System.out.println("Congratulations");
+			boardGame = board.showMines();
+			showBoard();
+		} else {
+			System.out.println("Mine! You lost!");
+			boardGame = board.showMines();
+			showBoard();
+		}
+	}
+
 	public static void main(String[] args) {
 		Minesweeper game = new Minesweeper();
+		game.selectCoordinates();
 	}
 }
